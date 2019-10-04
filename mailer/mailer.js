be49811423ee5e5b90af2d5f86d989707842d6a7
Jose Arguello <johao5991@gmail.com>
2019-10-04 13:04:37 -0400

    Agregando seguridad al usuario y mailer con nodemailer

---
+++ mailer/mailer.js
@@ -0,0 +1,8 @@
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "zachery46@ethereal.email",
+    pass: "k4qVH2uHwGn1M1V8G5"
+  }
+});
--- models/usuario.js
+++ models/usuario.js
@@ -1,11 +1,58 @@
 var mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 var Reserva = require("./reserva");
+const bcrypt = require("bc");
+const saltRounds = 10;
+
 var Schema = mongoose.Schema;
 ObjectId = Schema.ObjectId;
 
+const validateEmail = function(email) {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
 var usuarioSchema = new Schema({
-  nombre: String
+  nombre: {
+    type: String,
+    trim: true,
+    required: [true, "El nombre es obligatorio"]
+  },
+  email: {
+    type: String,
+    trim: true,
+    require: [true, "El email es obligatorio"],
+    lowercase: true,
+    unique: true,
+    validate: [validateEmail, "Por favor ingrese un email valido"],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  password: {
+    type: String,
+    required: [true, "El password es obligatorio"]
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  verificado: {
+    type: Boolean,
+    default: false
+  }
 });
+
+usuarioSchema.plugin(uniqueValidator, {
+  message: "El {PATH} ya existe con otro usuario."
+});
+
+usuarioSchema.pre("save", function(next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, saltRounds);
+  }
+  next();
+});
+
+usuarioSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb) {
   var reserva = new Reserva({
