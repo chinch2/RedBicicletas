520e5b81a38d6f6397b67f5cc72187d28f2d1a97
chinch2 <johao5991@gmail.com>
2019-10-14 17:08:07 -0400

    Bug fixes

--- models/usuario.js
+++ models/usuario.js
@@ -85,7 +85,7 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
       to: email_destination,
       subject: "Verificacion de cuenta",
       text:
-        "Hola, \n\n" +
+        "Hola guapa, \n\n" +
         "Por favor, para verificar su cuenta haga click en este link: \n" +
         "https://redbicis.herokuapp.com" +
         "/token/confirmation/" +
