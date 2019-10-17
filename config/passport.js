0be43e5a108a539b1a5a340867a3f32774c287c6
chinch2 <johao5991@gmail.com>
2019-10-17 16:30:34 -0400

    indentation

--- config/passport.js
+++ config/passport.js
@@ -4,15 +4,19 @@ const Usuario = require("../models/usuario");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.use(
-  new LocalStrategy(function(email, password, done) {
-    Usuario.findOne({ email: email }, function(err, usuario) {
+  new LocalStrategy(function (email, password, done) {
+    Usuario.findOne({
+      email: email
+    }, function (err, usuario) {
       if (err) return done(err);
       if (!usuario)
         return done(null, false, {
           message: "Email no existente o incorrecto"
         });
       if (!usuario.validPassword(password))
-        return done(null, false, { message: "Password incorrecto" });
+        return done(null, false, {
+          message: "Password incorrecto"
+        });
 
       return done(null, usuario);
     });
@@ -20,28 +24,27 @@ passport.use(
 );
 
 passport.use(
-  new GoogleStrategy(
-    {
+  new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.EMAIL_DOMAIN + "/auth/google/callback"
     },
-    function(accessToken, refreshToken, profile, cb) {
+    function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
 
-      Usuario.findOneOrCreateByGoogle(profile, function(err, user) {
+      Usuario.findOneOrCreateByGoogle(profile, function (err, user) {
         return cb(err, user);
       });
     }
   )
 );
 
-passport.serializeUser(function(usuario, cb) {
+passport.serializeUser(function (usuario, cb) {
   cb(null, usuario.id);
 });
 
-passport.deserializeUser(function(id, cb) {
-  Usuario.findById(id, function(err, usuario) {
+passport.deserializeUser(function (id, cb) {
+  Usuario.findById(id, function (err, usuario) {
     cb(err, usuario);
   });
 });
