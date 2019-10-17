a462be4d77f9344e00be9fa927131eec60aa4c29
chinch2 <johao5991@gmail.com>
2019-10-17 15:46:46 -0400

    Agregando API de Google+ y credenciales web

--- models/usuario.js
+++ models/usuario.js
@@ -11,7 +11,7 @@ const mailer = require("../mailer/mailer");
 var Schema = mongoose.Schema;
 ObjectId = Schema.ObjectId;
 
-const validateEmail = function(email) {
+const validateEmail = function (email) {
   const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return re.test(email);
 };
@@ -47,18 +47,18 @@ usuarioSchema.plugin(uniqueValidator, {
   message: "El {PATH} ya existe con otro usuario."
 });
 
-usuarioSchema.pre("save", function(next) {
+usuarioSchema.pre("save", function (next) {
   if (this.isModified("password")) {
     this.password = bcrypt.hashSync(this.password, saltRounds);
   }
   next();
 });
 
-usuarioSchema.methods.validPassword = function(password) {
+usuarioSchema.methods.validPassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
-usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb) {
+usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
   var reserva = new Reserva({
     usuario: this._id,
     bicicleta: biciId,
@@ -69,13 +69,13 @@ usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb) {
   reserva.save(cb);
 };
 
-usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
+usuarioSchema.methods.enviar_email_bienvenida = function (cb) {
   const token = new Token({
     _userId: this.id,
     token: crypto.randomBytes(16).toString("hex")
   });
   const email_destination = this.email;
-  token.save(function(err) {
+  token.save(function (err) {
     if (err) {
       return console.log(err.message);
     }
@@ -84,8 +84,7 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
       from: "no-reply@redbicicletas.com", //"Bicicletas El Arguellito <biciargu@gmail.com>"
       to: email_destination,
       subject: "Verificacion de cuenta",
-      text:
-        "Hola, \n\n" +
+      text: "Hola, \n\n" +
         "Por favor, para verificar su cuenta haga click en este link: \n" +
         process.env.EMAIL_DOMAIN +
         "/token/confirmation/" +
@@ -93,7 +92,7 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
         ".\n"
     };
 
-    mailer.sendMail(mailOptions, function(err) {
+    mailer.sendMail(mailOptions, function (err) {
       if (err) {
         return console.log(err.message);
       }
@@ -104,13 +103,13 @@ usuarioSchema.methods.enviar_email_bienvenida = function(cb) {
   });
 };
 
-usuarioSchema.methods.resetPassword = function(cb) {
+usuarioSchema.methods.resetPassword = function (cb) {
   const token = new Token({
     _userId: this.id,
     token: crypto.randomBytes(16).toString("hex")
   });
   const email_destination = this.email;
-  token.save(function(err) {
+  token.save(function (err) {
     if (err) {
       return cb(err);
     }
@@ -119,8 +118,7 @@ usuarioSchema.methods.resetPassword = function(cb) {
       from: "no-reply@redbicicletas.com",
       to: email_destination,
       subject: "Reseteo de password de cuenta",
-      text:
-        "Hola \n\n" +
+      text: "Hola \n\n" +
         "Por favor, para resetear el password de su cuenta haga click en este link: \n" +
         "http://localhost:3000" +
         "/resetPassword/" +
@@ -128,24 +126,52 @@ usuarioSchema.methods.resetPassword = function(cb) {
         ".\n"
     };
 
-    mailer.sendMail(mailOptions, function(err) {
+    mailer.sendMail(mailOptions, function (err) {
       if (err) {
         return cb(err);
       }
 
       console.log(
         "Se envio un email para resetear el password a: " +
-          email_destination +
-          "."
+        email_destination +
+        "."
       );
     });
     cb(null);
   });
 };
 
-usuarioSchema.statics.findOneOrCreateByGoogle = function findOneOrCreate(
-  condition,
-  callback
-) {};
+usuarioSchema.statics.findOneOrCreateByGoogle = function findOneOrCreate(condition, callback) {
+  const self = this;
+  console.log(condition);
+  self.findOne({
+    $or: [{
+      'googleId': condition.id
+    }, {
+      'email': condition.emails[0].value
+    }]
+  }, (err, result) => {
+    if (result) {
+      callback(err, result);
+    } else {
+      console.log('------------CONDITION-------------');
+      console.log(condition);
+      let values = {};
+      values.googleId = condition.id;
+      values.email = condition.emails[0].value;
+      values.nombre = condition.displayName || 'SIN NOMBRE';
+      values.verificado = true;
+      values.password = condition._json.etag;
+      console.log('------------VALUES----------');
+      console.log(values);
+      self.create(values, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        return callback(err, result);
+      })
+    }
+  })
+};
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
