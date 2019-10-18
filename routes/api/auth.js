9c98d9820d763c5b7197cd650bf1d2742fccbcff
chinch2 <johao5991@gmail.com>
2019-10-18 11:22:39 -0400

    agregando passport a auth api route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("../../config/passport");
 const authController = require("../../controllers/api/authControllerAPI");
 
 router.post("/authenticate", authController.authenticate);
